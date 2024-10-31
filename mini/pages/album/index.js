1cdc339d6e32e21ac57a7d160af8e87361f7c5bb
wb-jx721745 <wb-jx721745@alibaba-inc.com>
2024-10-31 17:13:45 +0800

    feat: 相册

--- mini/pages/album/index.js
+++ mini/pages/album/index.js
@@ -1,148 +1,66 @@
 // pages/album/index.js
-const { modal, toast, format } = require('../../utils/util');
-
-// 连接云数据库
-const db = wx.cloud.database();
-// 获取集合的引用
-const dbPhotoAlbum = db.collection('photoAlbum');
-// 数据库操作符
-const _ = db.command;
-
 Page({
-  mixins: [require('../../mixin/common')],
+
   /**
    * 页面的初始数据
    */
   data: {
-    showLoading: false,
-    page: 0,
-    list: [],
-    loadMoreStatus: 'more',
-    footerTip: ''
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
 
   },
 
   /**
-   * 页面数据初始化完成
+   * 生命周期函数--监听页面初次渲染完成
    */
-  initSuccess: function () {
-    this.loadAlbums();
-  },
-  onReachBottom: function () {
-    if (this.data.loadMoreStatus == 'more') {
-      this.loadAlbums();
-    }
+  onReady() {
+
   },
 
   /**
-   * 跳转到相册详情页
+   * 生命周期函数--监听页面显示
    */
-  toDetail(event){
-    let id = event.currentTarget.dataset.id;
-    wx.navigateTo({
-      url: '/model-album/pages/detail/index?id='+id
-    })
+  onShow() {
+
   },
 
   /**
-   * 跳转到创建相册页面
+   * 生命周期函数--监听页面隐藏
    */
-  toCreateAlbum(){
-    wx.navigateTo({
-      url: '/model-album/pages/setting/index',
-    })
+  onHide() {
+
   },
 
   /**
-   * 加载相册列表
+   * 生命周期函数--监听页面卸载
    */
-  loadAlbums() {
-    let _this = this;
-    let page = this.data.page;
-    if (!this.data.userInfo.auth_view && page == 1) {
-      this.setData({
-        footerTip: '游客只能查看20个相册',
-        loadMoreStatus: 'nomore'
-      })
-      return modal('提示', '游客只能查看20个相册');
-    }
-    this.setData({
-      loadMoreStatus: 'loading'
-    })
-    let size = 20;
-    let skip = page * size;
-    dbPhotoAlbum.limit(size)
-      .where(_.or([
-        {
-          member: _.in([this.data.userInfo._openid])
-        },
-        {
-          is_public: _.eq(1)
-        },
-        {
-          _openid: this.data.userInfo._openid
-        }
-      ]))
-      .skip(skip)
-      .orderBy('create_time', 'desc')
-      .get().then((res) => {
-        let list = res.data;
-        if (list.length >= 1) {
-          let setinfo = {};
-          list.forEach(element => {
-            element.time = format(element.create_time, "yyyy-MM-dd HH:mm");
-            if (element.alias == "") {
-              if (element.name == "common") {
-                element.alias = "动态相册";
-              } else {
-                element.alias = "未命名";
-              }
-            } else if (element.name == "common") {
-              element.alias += "-动态";
-            }
-          });
-          if (page == 0) {
-            setinfo['list'] = list;
-          } else {
-            setinfo['list'] = [..._this.data.list, ...list];
-          }
-          setinfo['page'] = page += 1
-          setinfo['showLoading'] = false;
-          setinfo['loadMoreStatus'] = 'more';
-          _this.setData(setinfo);
-          dbPhotoAlbum.where(_.or([
-            {
-              member: _.in([this.data.userInfo._openid])
-            },
-            {
-              is_public: _.eq(1)
-            },
-            {
-              _openid: this.data.userInfo._openid
-            }
-          ])).count().then(res=>{
-            if (res.total == list.length) {
-              _this.setData({
-                footerTip: '没有更多了',
-                loadMoreStatus: 'nomore'
-              })
-            }
-          })
-        } else {
-          toast("没有更多了");
-          _this.setData({
-            footerTip: '没有更多了',
-            loadMoreStatus: 'nomore'
-          })
-        }
-      }).catch((e) => {
-        toast("数据加载失败", e);
-      });
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
   }
 })
